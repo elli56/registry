fca9a5d34b53afab0201811e9fb57db815d95384
elli56 <111000010110001001e@gmail.com>
2021-06-27 16:18:53 +0300

    added dynamic routing for dashboard

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, url_for, redirect, request, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import login_user, login_required, LoginManager, logout_user, UserMixin
+from flask_login import login_user, login_required, LoginManager, logout_user, UserMixin, current_user
 from flask_bcrypt import Bcrypt
 
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -17,6 +18,19 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.LargeBinary(), nullable=False)
+    entries = db.relationship("Entrie", backref="owner")
+
+class Entrie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(150), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"ID:{self.id}, title:{self.title}"
+
 
 db.create_all()
 
@@ -42,7 +56,7 @@ def login():
     if user:
         if bcrypt.check_password_hash(user.password, password):
             login_user(user)
-            return redirect(url_for('dashboard'))
+            return redirect(url_for('dashboard', id = current_user.id))
         else:
             return redirect(url_for('login'))
     return render_template('login.html')
@@ -75,17 +89,40 @@ def register():
     return render_template('register.html')
 
 
-@app.route('/dashboard')
+@app.route('/dashboard/<int:id>')
 @login_required
 def dashboard():
-    return render_template('dashboard.html')
+    user = current_user
+
+    return render_template('dashboard.html', user=user)
 
 
 
+@app.route('/create-entrie', methods=['GET', 'POST'])
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        content = request.form['content']
+
+        new_entrie = Entrie(title=title, description=description, content=content, user_id=current_user.id)
+        db.session.add(new_entrie)
+        db.session.commit()
+        return redirect('/dashboard')
+
+    return render_template('create.html')
 
 
 
+@app.after_request
+def redirect_to_signin(response):  #если пользователь будет ломиться на страничку куда нельзя без авторизации 
+    # мы будем сразу его перенапрвлять на старничку с логином после чего будем перенаправлять туда куда он хотел попасть
+    #response это тот ответ который даеют любой метод куда нельзя например код 401
+    if response.status_code == 404:
+        return redirect(url_for('login'))
 
+    return response 
 
 
 
